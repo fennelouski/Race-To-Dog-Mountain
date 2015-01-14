6731f801c49583c21b4b8fd1aeda16ba22f14537
developerNathan <nathan@mydonebox.com>
2015-01-14 16:35:44 -0500

    Initial Commit

---
+++ Race to Dog Mountain/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Race to Dog Mountain
+//
+//  Created by Developer Nathan on 1/14/15.
+//  Copyright (c) 2015 Nathan Fennel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Race to Dog MountainTests/Race_to_Dog_MountainTests.m
@@ -0,0 +1,40 @@
+//
+//  Race_to_Dog_MountainTests.m
+//  Race to Dog MountainTests
+//
+//  Created by Developer Nathan on 1/14/15.
+//  Copyright (c) 2015 Nathan Fennel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Race_to_Dog_MountainTests : XCTestCase
+
+@end
+
+@implementation Race_to_Dog_MountainTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
